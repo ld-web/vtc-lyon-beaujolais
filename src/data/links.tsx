29ec64fb8a8fa6270d57422e6c57b030198cb338
Lucas <lucas@ld-web.net>
2022-05-31 20:28:33 +0200

    :zap: Fragmented links into multiple exports

--- src/data/links.tsx
+++ src/data/links.tsx
@@ -3,27 +3,27 @@ interface Link {
   label: string;
 }
 
-const links: Link[] = [
-  {
-    to: "/",
-    label: "Accueil",
-  },
-  {
-    to: "/a-propos",
-    label: "A propos",
-  },
-  {
-    to: "/nos-prestations",
-    label: "Nos prestations",
-  },
-  {
-    to: "/nos-tarifs",
-    label: "Nos tarifs",
-  },
-  {
-    to: "/contact",
-    label: "Contact & Réservation",
-  },
-];
+export const HOME: Link = {
+  to: "/",
+  label: "Accueil",
+};
+export const ABOUT: Link = {
+  to: "/a-propos",
+  label: "À propos",
+};
+export const PRESTATIONS: Link = {
+  to: "/nos-prestations",
+  label: "Nos prestations",
+};
+export const TARIFS: Link = {
+  to: "/nos-tarifs",
+  label: "Nos tarifs",
+};
+export const CONTACT: Link = {
+  to: "/contact",
+  label: "Contact & Réservation",
+};
+
+const links: Link[] = [HOME, ABOUT, PRESTATIONS, TARIFS, CONTACT];
 
 export default links;
