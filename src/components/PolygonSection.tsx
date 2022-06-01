f4524f1c29143604d568c7460872f761795bf651
Lucas <lucas@ld-web.net>
2022-06-01 13:19:42 +0200

    :lipstick: Adapted font size in polygon section

--- src/components/PolygonSection.tsx
+++ src/components/PolygonSection.tsx
@@ -7,7 +7,12 @@ interface PolygonSectionProps extends PolygonProps {
 }
 
 const SectionHeading = styled.h2`
+  font-size: 1.8rem;
   margin-bottom: 0;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    font-size: 2rem;
+  }
 `;
 
 export default function PolygonSection({
