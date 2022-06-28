6ae32afe6d7d93953133ecd4a4c43a116ad3758c
Lucas <lucas@ld-web.net>
2022-06-29 01:11:19 +0200

    :bento: add images and tel/email to contact page

--- src/pages/contact-reservation.tsx
+++ src/pages/contact-reservation.tsx
@@ -1,8 +1,10 @@
 import { PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
 import ContactForm from "../components/Contact/ContactForm";
+import { Desktop } from "../utils/rwd";
 
 const ContactContainer = styled.div`
   display: flex;
@@ -17,6 +19,50 @@ const ContactContainer = styled.div`
       flex-basis: 50%;
     }
   }
+`;
+
+const CtaContactContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.main};
+  color: #fff;
+  padding: 0.7rem 1rem;
+  text-align: left;
+
+  p {
+    margin-top: 0;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  a {
+    display: inline-block;
+    font-size: initial;
+    padding: 0.4rem 0.8rem;
+    color: white;
+    background-color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+    text-transform: uppercase;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #00a98f;
+    }
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    padding: 1.6rem 2rem;
+    font-size: large;
+
+    a {
+      padding: 0.8rem 1.4rem;
+      font-size: large;
+    }
+  }
+`;
+
+const ImagesContainer = styled.div`
+  display: flex;
 `;
 
 const ContactReservation = ({ location }: PageProps) => {
@@ -37,8 +83,46 @@ const ContactReservation = ({ location }: PageProps) => {
             mesure simplement par téléphone/SMS/e mail ou via notre formulaire
             de réservation.
           </p>
+          <CtaContactContainer>
+            <p>
+              Email
+              <br />
+              <a href="mailto:antoineadeline.vtc@gmail.com">
+                antoineadeline.vtc@gmail.com
+              </a>
+            </p>
+            <p>
+              Téléphone
+              <br />
+              <a href="tel:+33659324617">0659324617</a>
+            </p>
+          </CtaContactContainer>
         </div>
       </ContactContainer>
+      <ImagesContainer className="content">
+        <Desktop>
+          <StaticImage
+            src="../images/contact-images/fourviere_hotel.jpg"
+            alt="Antoine & Adeline transport VTC, traversez l'horizon avec nous !"
+            placeholder="blurred"
+            layout={"constrained"}
+          />
+        </Desktop>
+        <StaticImage
+          src="../images/contact-images/Bocuse_1.jpg"
+          alt="Antoine & Adeline transport VTC, traversez l'horizon avec nous !"
+          placeholder="blurred"
+          layout={"constrained"}
+        />
+        <Desktop>
+          <StaticImage
+            src="../images/contact-images/mur_lyonnais.jpg"
+            alt="Antoine & Adeline transport VTC, traversez l'horizon avec nous !"
+            placeholder="blurred"
+            layout={"constrained"}
+          />
+        </Desktop>
+      </ImagesContainer>
     </>
   );
 };
